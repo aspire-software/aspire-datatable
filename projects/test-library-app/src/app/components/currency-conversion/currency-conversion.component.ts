376ee512991ec1b6dbec1df5bb21174402df18a4
Ajay Charotariya <ajay.charotariya@aspiresoftserv.com>
2020-04-19 15:28:39 +0530

    fixed selected in select options

--- projects/test-library-app/src/app/components/currency-conversion/currency-conversion.component.ts
+++ projects/test-library-app/src/app/components/currency-conversion/currency-conversion.component.ts
@@ -19,6 +19,8 @@ export class CurrencyConversionComponent implements OnInit {
   public apisSource: any;          // apis source for source currencies rates dropdown
   public disabled: boolean;        // disable calender icon 
   public selectedSource: string;   // selected source
+  public selectedBase: string;
+  public selectedTarget: string;
   public submitted: boolean;       // handle form submit
   public apisDetails: any;         // api configurations 
   public host: any;                // host api
@@ -33,6 +35,7 @@ export class CurrencyConversionComponent implements OnInit {
     this.isVisible = false; 
     this.disabled = true;
     this.dateError = false;
+    this.currencyRates = [];
 
     /* Make form value blank */
     this.myForm = new FormGroup({
@@ -49,14 +52,15 @@ export class CurrencyConversionComponent implements OnInit {
     this.apisSource = this.apisDetails.currencyRateSource
 
     
-    // Get latest currencies rate [ default base: USD ]
+    /* Get latest currencies rate [ default base: USD ] */
     this.currencyConversionService.getCurrencyRates(`${this.host}/latest?base=USD`).subscribe(data => {
       if (data) {
         for (var key in data['rates']) {
           if (data['rates'].hasOwnProperty(key)) {
-            this.currencyRates.push({ currencyCode: key, rate: data['rates'][key], defaultBase: data.base })
+            this.currencyRates.push(Object.assign({}, { currencyCode: key, rate: data['rates'][key], defaultBase: data.base }));
           }
         }
+        this.sortCurrencyCode(this.currencyRates)
       }
     }, error => {
       console.log("Error", error)
@@ -72,28 +76,40 @@ export class CurrencyConversionComponent implements OnInit {
       this.disabled = false;
     } else {
       /* Get latest currencies rate by selection of latest option [ default base: USD ] */
+      this.myForm.controls['datePicker'].setValue(undefined);
       this.myForm.controls['datePicker'].disable();
       this.disabled = true;
+      this.currencyRates = [];
       this.currencyConversionService.getCurrencyRates(`${this.host}/latest?base=USD`).subscribe(data => {
         if (data) {
           for (var key in data['rates']) {
             if (data['rates'].hasOwnProperty(key)) {
-              this.currencyRates.push({ currencyCode: key, rate: data['rates'][key], defaultBase: data.base })
+              this.currencyRates.push(Object.assign({}, { currencyCode: key, rate: data['rates'][key], defaultBase: data.base }));
             }
+            this.sortCurrencyCode(this.currencyRates)
           }
         }
       }, error => {
         console.log("Error", error)
       })
+
     }
+  }
+
+  onBaseCurrencyChange(event){
+    this.selectedBase = "";
+    this.selectedBase = event.target.options[event.target.options.selectedIndex].text
+  }
+
+  onTargetCurrencyChange(event){
+    this.selectedTarget = "";
+    this.selectedTarget = event.target.options[event.target.options.selectedIndex].text
   }
 
   /* On date change get currencies rate of specified date  */
   async onDateChange(model) {
     this.dateError = false;
     if (model !== undefined) {
-      this.currencyRates = [];
-
       // Display error on selection of today & future dates [date picker]
      
       let getToday = this.calendar.getToday()
@@ -103,15 +119,17 @@ export class CurrencyConversionComponent implements OnInit {
       this.validateDate(selectedDate,todayDate)
       
       
+      this.currencyRates = [];
       // Get currency rate history of selected date
-      let url = `${this.host}/${date}`
+      let url = `${this.host}/${date}?base=USD`
       this.currencyConversionService.getCurrencyRates(url).subscribe(data => {
         if (data) {
           for (var key in data['rates']) {
             if (data['rates'].hasOwnProperty(key)) {
-              this.currencyRates.push({ currencyCode: key, rate: data['rates'][key], defaultBase: data.base })
+              this.currencyRates.push(Object.assign({}, { currencyCode: key, rate: data['rates'][key], defaultBase: data.base }));
             }
           }
+          this.sortCurrencyCode(this.currencyRates)
         }
       }, error => {
         console.log("Error", error)
@@ -157,16 +175,27 @@ export class CurrencyConversionComponent implements OnInit {
         return;
       }
       let amount = form.value.amount;
-      let baseCurrencyRate = form.value.countryRates.rate;
-      let baseCurrencyCode = form.value.countryRates.currencyCode;
-      let targetCurrencyRate = form.value.targetRates.rate;
-      let targetCurrencyCode = form.value.targetRates.currencyCode;
+      let baseCurrencyRate = form.value.countryRates;
+      let baseCurrencyCode = this.selectedBase;
+      let targetCurrencyRate = form.value.targetRates;
+      let targetCurrencyCode = this.selectedTarget;
       this.convertedRates = await this.currencyConversionService.convertCurrency(amount, baseCurrencyRate, baseCurrencyCode, targetCurrencyRate, targetCurrencyCode);
+      this.submitted = false;
     }
+  }
+
+  sortCurrencyCode(data){
+    data.sort(function(a, b){
+      if(a.currencyCode < b.currencyCode) { return -1; }
+      if(a.currencyCode > b.currencyCode) { return 1; }
+      return 0;
+  })
   }
 
   /* Reset form after submit values */
   resetForm() {
+    this.selectedBase = undefined
+    this.selectedTarget = undefined
     this.isVisible = false;
     this.submitted = false;
     this.myForm = new FormGroup({
