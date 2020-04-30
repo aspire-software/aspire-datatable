0314a4ecd9bdb1024a2fb9ae6202b290289f8796
Dipesh <dipesh.raichana@aspiresoftware.in>
2020-04-30 20:13:18 +0530

    added github link and removed spaces

--- projects/angular-currency-converter/src/public-api.ts
+++ projects/angular-currency-converter/src/public-api.ts
@@ -1,6 +1,5 @@
 /*
  * Public API Surface of angular-currency-converter
  */
-
 // export * from './services/services.module';
 export * from './services/currency-conversion.service';
--- projects/angular-currency-converter/src/services/currency-conversion.service.ts
+++ projects/angular-currency-converter/src/services/currency-conversion.service.ts
@@ -3,47 +3,37 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, pipe, throwError } from 'rxjs';
 import { map,catchError } from 'rxjs/operators';
 import { apisConfigurations } from '../shared/config'
-
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyConversionService {
-
   constructor(private http: HttpClient) { }
   public url: string;
   public convertedRates: any = [];  //Store converted values 
   public apisConfigurations: any = apisConfigurations;
-
   /* 
   Get currency apis configurations
   */
-
   getApisConfigurations() {
     return pipe(this.apisConfigurations)
   }
-
   /* 
   Get currency rate from specified url
   */
-
   getCurrencyRates(url: string): Observable<any> {
     return this.http.get(url)
       .pipe(map((data: Response) => data,catchError(this.handleError)))
   }
-
   handleError(error: HttpErrorResponse) {
     return throwError(error);
   }
-
   /* 
   convert currencies & accept parameter of amount, baseCurrencyRate , baseCurrencyCode, targetCurrencyRate, targetCurrencyCode
   */
   convertCurrency(amount: number, baseCurrencyRate: number, baseCurrencyCode: string, targetCurrencyRate: number, targetCurrencyCode: string) {
     this.convertedRates = []; // Clear array before convert currencies
-
     let baseCurrencyRates: any = baseCurrencyRate; // Selected base currency rate
-    let targetCurrencyRates: any = targetCurrencyRate; // Selected target currency rate
-    
+    let targetCurrencyRates: any = targetCurrencyRate; // Selected target currency rate 
     let countryCurrencyCode: any = targetCurrencyCode; // Targeted currency code
     let baseRate = parseFloat(baseCurrencyRates).toFixed(2); // // Fixed to two decimal number
     let rates: any = parseFloat(targetCurrencyRates).toFixed(2); 
